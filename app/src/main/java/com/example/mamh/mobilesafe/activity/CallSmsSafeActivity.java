e4c6e7fd17b492ef627be47f016794c41aa416b5
Minghui Ma <mmh19891113@qq.com>
2015-12-06 20:03:42 +0800

    fixed listview display error
    应该在getView（）方法里

--- app/src/main/java/com/example/mamh/mobilesafe/activity/CallSmsSafeActivity.java
+++ app/src/main/java/com/example/mamh/mobilesafe/activity/CallSmsSafeActivity.java
@@ -1,11 +1,10 @@
 package com.example.mamh.mobilesafe.activity;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -16,6 +15,7 @@ import com.example.mamh.mobilesafe.domain.BlackNumberInfo;
 import java.util.List;
 
 public class CallSmsSafeActivity extends Activity {
+    private static final String TAG = "CallSmsSafeActivity";
     private ListView lv_callsms_safe;
     private List<BlackNumberInfo> infos;
     private BlackNumberDao dao;
@@ -43,19 +43,7 @@ public class CallSmsSafeActivity extends Activity {
 
         @Override
         public Object getItem(int position) {
-            View view = View.inflate(getApplicationContext(), R.layout.list_item, null);
-            TextView tv_black_number = (TextView) view.findViewById(R.id.tv_black_number);
-            TextView tv_mode = (TextView) view.findViewById(R.id.tv_mode);
-            tv_black_number.setText(infos.get(position).getNumber());
-            String mode = infos.get(position).getMode();
-            if("1".equals(mode)){
-                tv_mode.setText("电话拦截");
-            }else if("2".equals(mode)){
-                tv_mode.setText("短息拦截");
-            }else{
-                tv_mode.setText("其他拦截");
-            }
-            return view;
+            return null;
         }
 
         @Override
@@ -65,7 +53,19 @@ public class CallSmsSafeActivity extends Activity {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            return null;
+            View view = View.inflate(getApplicationContext(), R.layout.list_item, null);
+            TextView tv_black_number = (TextView) view.findViewById(R.id.tv_black_number);
+            TextView tv_mode = (TextView) view.findViewById(R.id.tv_mode);
+            tv_black_number.setText(infos.get(position).getNumber());
+            String mode = infos.get(position).getMode();
+            if ("1".equals(mode)) {
+                tv_mode.setText("电话拦截");
+            } else if ("2".equals(mode)) {
+                tv_mode.setText("短息拦截");
+            } else {
+                tv_mode.setText("其他拦截");
+            }
+            return view;
         }
     }
 }
