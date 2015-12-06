c91f5a5e8a77455c0430880a5479d021655d9943
Minghui Ma <mmh19891113@qq.com>
2015-12-06 20:36:22 +0800

    improve listview use viewholder
    Signed-off-by: Minghui Ma <mmh19891113@qq.com>

--- app/src/main/java/com/example/mamh/mobilesafe/activity/CallSmsSafeActivity.java
+++ app/src/main/java/com/example/mamh/mobilesafe/activity/CallSmsSafeActivity.java
@@ -53,25 +53,41 @@ public class CallSmsSafeActivity extends Activity {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
             //这个回去读取流xml文件，然后去解析，最后才生成我们需要的view，这样很消耗资源的。怎么优化？？？
             View view;
             if (convertView == null) {
                 view = View.inflate(getApplicationContext(), R.layout.list_item, null);
+                //减少findViewById操作。减少子view的查询次数。
+                holder = new ViewHolder();
+                holder.tv_black_number = (TextView) view.findViewById(R.id.tv_black_number);
+                holder.tv_mode = (TextView) view.findViewById(R.id.tv_mode);
+
+                // 把子view的引用放到父view对象里
+                view.setTag(holder);
             } else {
                 view = convertView;
+                holder = (ViewHolder) view.getTag();
             }
-            TextView tv_black_number = (TextView) view.findViewById(R.id.tv_black_number);
-            TextView tv_mode = (TextView) view.findViewById(R.id.tv_mode);
-            tv_black_number.setText(infos.get(position).getNumber());
+
+
+            holder.tv_black_number.setText(infos.get(position).getNumber());
             String mode = infos.get(position).getMode();
             if ("1".equals(mode)) {
-                tv_mode.setText("电话拦截");
+                holder.tv_mode.setText("电话拦截");
             } else if ("2".equals(mode)) {
-                tv_mode.setText("短息拦截");
+                holder.tv_mode.setText("短息拦截");
             } else {
-                tv_mode.setText("其他拦截");
+                holder.tv_mode.setText("其他拦截");
             }
             return view;
         }
     }
+
+    //记录view的内存地址
+    private class ViewHolder {
+        public TextView tv_black_number;
+        public TextView tv_mode;
+    }
+
 }
