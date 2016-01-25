4bbfac37e51d4f4b36f964b9405fd27a5b61f6ec
Minghui Ma <mmh19891113@qq.com>
2016-01-25 21:13:49 +0800

    add Telephone interception in CallSmsSafeService.java
    添加了 电话拦截 的功能
    里面用到了 通过反射来获取TELEPHONY_SERVICE，或去Ibinder
    还用到了aidl文件。
    
    Signed-off-by: Minghui Ma <mmh19891113@qq.com>

--- app/src/main/java/com/example/mamh/mobilesafe/service/CallSmsSafeService.java
+++ app/src/main/java/com/example/mamh/mobilesafe/service/CallSmsSafeService.java
@@ -6,10 +6,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.IBinder;
+import android.telephony.PhoneStateListener;
 import android.telephony.SmsMessage;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 
+import com.android.internal.telephony.ITelephony;
 import com.example.mamh.mobilesafe.db.dao.BlackNumberDao;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class CallSmsSafeService extends Service {
 
@@ -19,6 +25,8 @@ public class CallSmsSafeService extends Service {
     private InnerSmsReceiver innerSmsReceiver;
     private BlackNumberDao dao;
 
+    private TelephonyManager telephonyManager;
+    private CallPhoneStateListener callPhoneStateListener;
 
     public CallSmsSafeService() {
     }
@@ -32,6 +40,8 @@ public class CallSmsSafeService extends Service {
     public void onDestroy() {
         unregisterReceiver(innerSmsReceiver);
         innerSmsReceiver = null;
+
+        telephonyManager.listen(callPhoneStateListener, PhoneStateListener.LISTEN_NONE);//不监听
         super.onDestroy();
     }
 
@@ -45,8 +55,53 @@ public class CallSmsSafeService extends Service {
 
         dao = new BlackNumberDao(this);
 
+        callPhoneStateListener = new CallPhoneStateListener();
+
+        telephonyManager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+        telephonyManager.listen(callPhoneStateListener, PhoneStateListener.LISTEN_CALL_STATE);
     }
 
+
+    private class CallPhoneStateListener extends PhoneStateListener {
+        @Override
+        public void onCallStateChanged(int state, String incomingNumber) {
+            switch (state) {
+                case TelephonyManager.CALL_STATE_RINGING:
+                    String result = dao.findMode(incomingNumber);
+                    Log.e(TAG, "电话拦截，这里要挂断电话 ==  " + result + "===" + incomingNumber);
+                    if ("1".equals(result) || "3".equals(result)) {
+                        //电话拦截，这里要挂断电话
+                        Log.e(TAG, "电话拦截，这里要挂断电话 ");
+
+
+                        //挂断电话
+                        endCall();
+                    }
+                    break;
+            }
+
+
+            //super.onCallStateChanged(state, incomingNumber);
+            //当呼叫状态发生变化时候调用
+        }
+
+    }
+
+    private void endCall() {
+
+        // serviceManager 类是隐藏，这里不能直接获得，需要用到反射来获取
+        // IBinder iBinder = ServiceManager.getService();
+
+        try {
+            Class clazz = CallSmsSafeService.class.getClassLoader().loadClass("android.os.ServiceManager");
+            Method method = clazz.getDeclaredMethod("getService", String.class);
+            IBinder iBinder = (IBinder) method.invoke(null, TELEPHONY_SERVICE);
+
+            ITelephony.Stub.asInterface(iBinder).endCall();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     private class InnerSmsReceiver extends BroadcastReceiver {
 
