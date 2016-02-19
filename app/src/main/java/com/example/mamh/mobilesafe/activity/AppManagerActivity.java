f2409b43c7f060a9dc6ceb12f8947a04db8b1ce5
Minghui Ma <839682048@qq.com>
2016-02-19 10:32:13 +0800

    show app infos in a listview
    在listview中显示应用程序的信息，
    这里只是粗略的有个界面显示而已
    
    Signed-off-by: Minghui Ma <839682048@qq.com>

--- app/src/main/java/com/example/mamh/mobilesafe/activity/AppManagerActivity.java
+++ app/src/main/java/com/example/mamh/mobilesafe/activity/AppManagerActivity.java
@@ -5,15 +5,29 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.os.StatFs;
 import android.text.format.Formatter;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.mamh.mobilesafe.R;
+import com.example.mamh.mobilesafe.domain.AppInfo;
+import com.example.mamh.mobilesafe.engine.AppInfoProvider;
+
+import java.util.List;
 
 public class AppManagerActivity extends Activity {
 
     private TextView mAvailRom;
     private TextView mAvailSD;
 
+    private ListView lv_app_manger;
+    private LinearLayout ll_loading;
+
+
+    private List<AppInfo> appInfos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +42,26 @@ public class AppManagerActivity extends Activity {
         mAvailRom.setText("可用空间:" + Formatter.formatFileSize(this, romsize));
         mAvailSD.setText("sd卡可用空间:" + Formatter.formatFileSize(this, sdsize));
 
+        lv_app_manger = (ListView) findViewById(R.id.lv_app_manger);
+        ll_loading = (LinearLayout) findViewById(R.id.ll_loading);
+
+        //这里，如果应用多了，这里会是一个耗时的操作，要写在子线程里面
+        //appInfos = AppInfoProvider.getAppInfo(this);
+
+        ll_loading.setVisibility(View.VISIBLE);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                appInfos = AppInfoProvider.getAppInfo(AppManagerActivity.this);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        lv_app_manger.setAdapter(new AppManagerAdapter());
+                        ll_loading.setVisibility(View.INVISIBLE);
+                    }
+                });
+            }
+        }).start();
     }
 
 
@@ -45,5 +79,32 @@ public class AppManagerActivity extends Activity {
 
         return size * count;
     }
+
+
+    private class AppManagerAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return appInfos.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            TextView tv = new TextView(getApplicationContext());
+            tv.setText(appInfos.get(position).toString());
+            return tv;
+        }
+    }
+
 
 }
